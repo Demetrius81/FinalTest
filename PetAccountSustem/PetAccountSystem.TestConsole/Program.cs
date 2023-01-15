c8fe86e718ef632f8d341ba66e69d4e7c2a0cf81
Dmitri <Shizimaniak@gmail.com>
2023-01-15 16:34:07 +0300

    Протестирована в консоли работа API и клиента

--- PetAccountSustem/PetAccountSystem.TestConsole/Program.cs
+++ PetAccountSustem/PetAccountSystem.TestConsole/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace PetAccountSystem.TestConsole;
+﻿using Microsoft.Extensions.Logging;
+using PetAccountSystem.Client.Pets;
+
+namespace PetAccountSystem.TestConsole;
 
 internal class Program
 {
@@ -6,6 +9,36 @@ internal class Program
 
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var client = new HttpClient();
+
+        client.BaseAddress = new Uri("http://localhost:5241");
+
+        var petClient = new PetsClient(client);
+
+        var all = petClient.GetAllAsync().Result;
+
+        var byId = petClient.GetByIDAsync(4).Result;
+
+        var count = petClient.CountAsync().Result;
+
+        var delete = petClient.DeleteAsync(byId).Result;
+
+        all = petClient.GetAllAsync().Result;
+
+        byId = petClient.GetByIDAsync(1).Result;
+
+        byId.Count = 9999;
+
+        var update = petClient.UpdateAsync(byId).Result;
+
+        byId = petClient.GetByIDAsync(1).Result;
+
+        byId.KindOfAnimal = "FFFFFFFFFFFF";
+
+        var added = petClient.AddAsync(byId).Result;
+
+        all = petClient.GetAllAsync().Result;
+
+        Console.ReadKey(true);
     }
 }
