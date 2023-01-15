7205a3825a447881db29a531ad8f7e539e028f84
Dmitri <Shizimaniak@gmail.com>
2023-01-15 16:34:48 +0300

    Создана инфраструктура WPF приложения.

---
+++ PetAccountSustem/PetAccountSystem.AppWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PetAccountSystem.AppWPF.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetAccountSystem.AppWPF.ViewModels;
+internal class MainWindowViewModel : ViewModel
+{
+    private string _title = "Система учета питомника животных";
+
+    /// <summary>Заголовок окна</summary>
+    public string Title
+    {
+        get => _title;
+        set => Set(ref _title, value);
+    }
+}
